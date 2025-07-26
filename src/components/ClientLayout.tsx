f754c8b7349f4729914ee4d9862f8f138a5c601d
geehyness <geehyness22@gmail.com>
2025-07-26 10:14:17 +0200

    fix

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -18,6 +18,7 @@ export default function ClientLayout({
 }) {
     const pathname = usePathname();
     const isDashboardPage = pathname.startsWith('/admin') || pathname.startsWith('/dashboard');
+    const isHomeViewerPage = pathname === '/home-viewer'; // Check if it's the home-viewer page
     const navbarType = isDashboardPage ? 'dashboard' : 'customer';
 
     return (
@@ -27,10 +28,14 @@ export default function ClientLayout({
                 appName={siteTitle}
                 siteLogoUrl={siteLogoUrl}
             />
-            <Box pt="64px" flex="1">
+            <Box
+                pt={isHomeViewerPage ? '0' : '64px'} // Apply pt="64px" only if NOT the home-viewer page
+                flex="1"
+            >
                 {children}
             </Box>
-            <Footer appName={siteTitle} />
+            {/* Render Footer only if NOT the home-viewer page */}
+            {!isHomeViewerPage && <Footer appName={siteTitle} />}
         </>
     );
 }
